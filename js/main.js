eab1fdf8ce1fcc2adf2ef8e2376fdffa56841dfd
Kirill Khomenko <kkhomenko10@gmail.com>
2023-03-12 23:49:00 +0100

    add form validation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 'use strict';
 
-const REG_EXPS = {
+//
+
+const INPUTS_REG_EXP = {
   'first-name': /^[A-Z][a-z]{2,19}(-[A-Z][a-z]{2,19})?$/,
   'last-name': /^[A-Z][a-z]{2,19}(-[A-Z][a-z]{2,19})?$/,
+  email: /^.+@.+\.[a-z]{2,3}$/,
+  'phone-number': /^\d{3}$/,
 };
 
-function validateName(e) {
-  if (REG_EXPS['first-name'].test(e.target)) {
+const formInputs = document.querySelectorAll('.formInput');
+
+formInputs.forEach((i) => i.addEventListener('input', inputHandler));
+
+function inputHandler(e) {
+  e.preventDefault();
+
+  if (INPUTS_REG_EXP[e.target.name].test(e.target.value)) {
     e.target.classList.add('valid');
     e.target.classList.remove('invalid');
   } else {
-    e.target.classList.add('invalid');
     e.target.classList.remove('valid');
+    e.target.classList.add('invalid');
   }
 }
